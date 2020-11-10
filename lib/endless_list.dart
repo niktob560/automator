2a3e1b5bd93496501eecb7300399a22632339548
nikto_b <niktob560@gmail.com>
2020-11-10 23:26:07 +0300

    Fixed endless list not loading if first _loadData call was returned not enough data to fill first screen of a device, but data already exists and not loading; added empty filler param

--- lib/endless_list.dart
+++ lib/endless_list.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:automator/misc.dart';
 
-class EndlessListStatefulWidget<T> extends StatefulWidget {
-  final Function _elementConstructor;
-  final Function _itemConstructor;
 
-  EndlessListStatefulWidget(this._elementConstructor, this._itemConstructor,
-      {Key key})
-      : super(key: key);
-
-  @override
-  State createState() =>
-      EndlessListStatefulWidgetState<T>(_elementConstructor, _itemConstructor);
-}
-
-enum _State { idle, loading, failed }
+enum _State { idle, loading, failed, done }
 
 typedef _ElementBuilder<T> = Future<Set<T>> Function(int alreadyDisplayed);
 typedef _ItemBuilder<T> = Widget Function(BuildContext context, T element);
+
+class EndlessListStatefulWidget<T> extends StatefulWidget {
+  /// (already) async => load(offset:already, size:S)
+  /// <T>[S]
+  final _ElementBuilder<T> _elementConstructor;
+
+  /// (context, T element) => Widget
+  final _ItemBuilder<T> _itemConstructor;
+
+  final Widget _emptyWidget;
+
+  EndlessListStatefulWidget(this._elementConstructor, this._itemConstructor,
+      [this._emptyWidget = const SizedBox(width: 0, height: 0), Key key])
+      : super(key: key);
+
+  @override
+  State createState() => EndlessListStatefulWidgetState<T>(
+      _elementConstructor, _itemConstructor, _emptyWidget);
+}
 
 class EndlessListStatefulWidgetState<T>
     extends State<EndlessListStatefulWidget> {
@@ -26,42 +34,61 @@ class EndlessListStatefulWidgetState<T>
 
   /// (context, T element) => Widget
   final _ItemBuilder<T> _itemConstructor;
+
+  final Widget _emptyWidget;
+
   final _all = <T>[];
+
+  double _screenExtent = 0;
 
   _State _state = _State.idle;
   ScrollController _scrollController;
   final _refreshIndicatorKey = GlobalKey<RefreshIndicatorState>();
 
   EndlessListStatefulWidgetState(
-      this._elementConstructor, this._itemConstructor);
+      this._elementConstructor, this._itemConstructor, this._emptyWidget);
 
   @override
   initState() {
+    super.initState();
     _scrollController = ScrollController()..addListener(_scrollListener);
     _loadData();
   }
 
   @override
   dispose() {
+    super.dispose();
     _scrollController.dispose();
   }
 
   _scrollListener() {
-    if (_state == _State.idle && _scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
+    print(
+        'SCROLL ${_scrollController.position.pixels} ${_scrollController.position.maxScrollExtent} ${_scrollController.offset}');
+    if (_screenExtent == 0 && _scrollController.position.pixels == 0) {
+      _screenExtent = _scrollController.position.maxScrollExtent;
+      _loadData();
+    } else if (_state == _State.idle &&
+        ((_scrollController.position.pixels + (_screenExtent * 2)) >=
+                _scrollController.position.maxScrollExtent ||
+            (_scrollController.position.maxScrollExtent ==
+                    _scrollController.position.pixels &&
+                _state == _State.idle))) {
       _loadData();
     }
   }
 
   Future<void> _refresh() async {
     _all.clear();
+    _state = _State.idle;
     _loadData();
   }
 
   _loadData() async {
+    if (_state == _State.done) return;
     setState(() {
       _state = _State.loading;
     });
-    var dat;
+    Set<T> dat;
     try {
       dat = await _elementConstructor(_all.length);
     } catch (e) {
@@ -71,15 +98,22 @@ class EndlessListStatefulWidgetState<T>
     }
     if (_state != _State.failed) {
       setState(() {
-        _all.addAll(dat);
-        _state = _State.idle;
+        if (dat.length != 0) {
+          for (var i in dat) {
+            if (!_all.contains(i)) {
+              _all.add(i);
+            }
+          }
+          _state = _State.idle;
+        } else
+          _state = _State.done;
       });
     }
   }
 
   @override
   Widget build(BuildContext context) =>
-      Container(child: _all.length != 0 ? _buildList() : Text('Wow, such empty'));
+      Container(child: _all.length != 0 ? _buildList() : _emptyWidget);
 
   _buildList() => RefreshIndicator(
         key: _refreshIndicatorKey,
@@ -88,11 +122,21 @@ class EndlessListStatefulWidgetState<T>
             controller: _scrollController,
             itemCount: _all.length + 1,
             physics: const AlwaysScrollableScrollPhysics(),
-            itemBuilder: (context, i) => i < _all.length
-                ? _itemConstructor(context, _all[i])
-                : ((_state == _State.loading)
-                    ? _loaderWidget()
-                    : const SizedBox(height: 4, width: 0))),
+            itemBuilder: (context, i) {
+              if (i >= _all.length && _state != _State.loading && _state != _State.done) {
+                    () async {
+                  await sleep1();
+                  _loadData();
+                }();
+                return _loaderWidget();
+              }
+              else
+                return i < _all.length
+                    ? _itemConstructor(context, _all[i])
+                    : ((_state == _State.loading)
+                        ? _loaderWidget()
+                        : const SizedBox(height: 4, width: 0));
+            }),
       );
 
   Widget _loaderWidget() => Align(
@@ -105,4 +149,6 @@ class EndlessListStatefulWidgetState<T>
         ),
         alignment: FractionalOffset.bottomCenter,
       );
+
+  get all => _all;
 }
